b423eadba9164050a35aabb24d04a8dcb71a8362
wongdarjo <bimaindramulya@gmail.com>
2022-02-13 11:18:56 +0700

    update resoponsive fontsize

--- src/components/SectionTitle/index.jsx
+++ src/components/SectionTitle/index.jsx
@@ -4,12 +4,12 @@ const SectionTitle = (props) => {
 	return (
 		<>
 			{props.type === 'h1' ? (
-				<h1 className='mb-4 font-heading text-2xl font-bold'>{props.children}</h1>
+				<h1 className='mb-4 font-heading text-xl font-bold xl:text-2xl'>{props.children}</h1>
 			) : (
-				<h2 className='mb-4 font-heading text-xl font-bold'>{props.children}</h2>
+				<h2 className='mb-4 font-heading text-lg font-bold xl:text-xl'>{props.children}</h2>
 			)}
 		</>
 	);
-}
+};
 
 export default SectionTitle;
