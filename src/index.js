5f0c3a5ea6ff9b763af1b4f686727d0f02b5dfc3
wongdarjo <bimaindramulya@gmail.com>
2022-02-14 06:18:11 +0700

    adding GTM setup

--- src/index.js
+++ src/index.js
@@ -3,6 +3,21 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import TagManager from 'react-gtm-module';
+
+const tagManagerArgs = {
+	gtmId: process.env.REACT_APP_GTM_ID,
+	auth:
+		process.env.NODE_ENV === 'production'
+			? process.env.REACT_APP_GTM_AUTH_PRODUCTION
+			: process.env.REACT_APP_GTM_AUTH_DEVELOPMENT,
+	preview:
+		process.env.NODE_ENV === 'production'
+			? process.env.REACT_APP_GTM_PREVIEW_PRODUCTION
+			: process.env.REACT_APP_GTM_PREVIEW_DEVELOPMENT
+};
+
+TagManager.initialize(tagManagerArgs);
 
 ReactDOM.render(
 	<React.StrictMode>
