0b6ffb4ab3cd3f7dec90a4b05f5a06b73d69ad7f
wongdarjo <bimaindramulya@gmail.com>
2022-02-13 11:19:08 +0700

    update header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-const Header = () => {
+const Header = ({ title = 'Random Image Bank' }) => {
 	return (
 		<header className='fixed top-0 z-50 w-full bg-slate-50 py-4 shadow-sm'>
 			<div className='container mx-auto flex items-center justify-between px-4'>
-				<span className='font-semibold tracking-wider'>Random Image</span>
+				<span className='font-semibold tracking-wider'>{title}</span>
 				<ul className='flex gap-x-4'>
 					<li>
 						<Link to='/' className='transition-colors hover:text-slate-400'>
